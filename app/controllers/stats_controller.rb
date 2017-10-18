c27c88fdd47c57f475babb97e4f0b563fa88537f
ldgarber <leah.garber88@gmail.com>
2017-10-18 17:13:50 -0400

    add most worn and never worn stats

---
+++ app/controllers/stats_controller.rb
@@ -0,0 +1,5 @@
+class StatsController < ApplicationController  
+  before_action :authenticate_user! 
+  def index
+  end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -14,4 +14,24 @@ class Item < ApplicationRecord
     long_name << self.name
     return long_name.split(" ").map{|word| word.capitalize}.join(" ")
   end
+
+  def worn_count
+    self.outfits.length
+  end
+
+  def self.most_worn(num)
+    Item.all.sort_by(&:worn_count).reverse.take(num)
+  end
+
+  def self.least_worn(num) 
+    Item.all.sort_by(&:worn_count).take(num)
+  end
+
+  def self.never_worn(num)
+    never_worn = []
+    Item.all.each do |item| 
+      never_worn << item if item.worn_count == 0
+    end
+    return never_worn.take(num)
+  end
 end
